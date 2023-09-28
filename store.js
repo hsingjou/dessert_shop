e606f05bbb30b840acc867c2b2da0f7044f8f631
hsungjou <shero011735@gmail.com>
2023-09-28 15:02:56 +0800

    0928-1

--- store.js
+++ store.js
@@ -16,3 +16,12 @@ function myFunction() {
       }       
     }
   }
+
+
+function show_menu_items(){
+    document.getElementById("show_menu_overlay").style.display="block";
+}
+
+function off_menu_overlay(){
+    document.getElementById("show_menu_overlay").style.display="none";
+}  

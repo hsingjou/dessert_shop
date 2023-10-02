eb4bd375d80c4beb9f1127eadbb4980e13b59af2
hsungjou <shero011735@gmail.com>
2023-10-02 16:27:46 +0800

    1002-??

--- menu.js
+++ menu.js
@@ -5,3 +5,11 @@ function show_menu_items(){
 function off_menu_overlay(){
     document.getElementById("show_menu_overlay").style.display="none";
 }
+
+function show_item1_overlay(){
+    document.getElementById("show_item1_overlay").style.display="block";
+}
+
+function off_item1_overlay(){
+    document.getElementById("show_item1_overlay").style.display="none";
+}
